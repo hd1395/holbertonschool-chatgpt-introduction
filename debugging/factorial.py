20830f3cab57b41d387d3da325a79b231529fde2
Hamad Dahash <48245047+hd1395@users.noreply.github.com>
2025-05-06 11:47:35 +0300

    fix the output

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -5,19 +5,8 @@ def factorial(n):
     result = 1
     while n > 1:
         result *= n
-        n -= 1  # Decrement n to avoid infinite loop
+        n -= 1
     return result
 
-# Get the number from command-line arguments
-if len(sys.argv) > 1:
-    try:
-        number = int(sys.argv[1])
-        if number < 0:
-            print("Factorial is not defined for negative numbers.")
-        else:
-            f = factorial(number)
-            print(f)
-    except ValueError:
-        print("Please provide a valid integer.")
-else:
-    print("Usage: python script.py <number>")
+f = factorial(int(sys.argv[1]))
+print(f)
